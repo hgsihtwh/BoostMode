cb8881cbada5be797c84a43e32d5e20fedc0dcf7
hgsihtwh <hgsihtwh@gmail.com>
2026-04-19 21:40:40 +0700

    feat: добавлена модель данных CircuitData

---
+++ app/src/main/java/com/example/boostmode/CircuitData.kt
@@ -0,0 +1,13 @@
+package com.example.boostmode
+
+data class CircuitData(
+    val name: String,
+    val gpName: String,
+    val round: String,
+    val dates: String,
+    val status: String,
+    val lapTime: String,
+    val turns: String,
+    val length: String,
+    val mapRes: Int
+)
