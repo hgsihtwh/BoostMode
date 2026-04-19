8bddb83ef73a3f6826c2e98f93334d0208e9f727
hgsihtwh <hgsihtwh@gmail.com>
2026-04-19 21:40:42 +0700

    feat: расширена модель данных CircuitData

--- app/src/main/java/com/example/boostmode/CircuitData.kt
+++ app/src/main/java/com/example/boostmode/CircuitData.kt
@@ -9,5 +9,15 @@ data class CircuitData(
     val lapTime: String,
     val turns: String,
     val length: String,
-    val mapRes: Int
+    val laps: String,
+    val mapRes: Int,
+    val circuitRes: Int,
+    val location: String,
+    val opened: String,
+    val firstF1Gp: String,
+    val about: String,
+    val driver: String,
+    val car: String,
+    val year: String,
+    val avgSpeed: String
 )
